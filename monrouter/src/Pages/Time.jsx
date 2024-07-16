d3222e4c7985bdaee7b1d9c81a4c4d3062f7400c
Ad15C <ad15canon@hotmail.com>
2024-07-16 08:30:09 +0200

    creation_pages

---
+++ monrouter/src/Pages/Time.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Time = () => {
+    return (
+        <div className="Time">
+            <h1>Heure</h1>
+            <p>Nous sommes aujourd'hui le:</p>
+        </div>
+    );
+}
+
+export default Time;
